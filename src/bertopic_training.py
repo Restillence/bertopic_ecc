60ea2a52ffb59acee9a31bdadf3c59cb59853ca0
Melius Vitae <65677840+Restillence@users.noreply.github.com>
2024-09-19 11:40:49 +0200

    changed heartbeat rate and smaller sample size

--- src/bertopic_training.py
+++ src/bertopic_training.py
@@ -201,7 +201,7 @@ class BertopicModel:
                 min_topic_size=self.config.get("min_topic_size", 15)  # Ensure smaller topics can be captured
             )
 
-    def _heartbeat(self, stop_event, interval=600):
+    def _heartbeat(self, stop_event, interval=300):
         """Periodically print a heartbeat message to keep the connection alive.
 
         Parameters
