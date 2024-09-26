e768ae1df0a2b50819795f057d51dd60422d809a
Melius Vitae <65677840+Restillence@users.noreply.github.com>
2024-09-26 15:59:27 +0200

    .

--- src/bertopic_training.py
+++ src/bertopic_training.py
@@ -300,7 +300,7 @@ def main():
     """
     # Load configuration from config.json
     print("Loading configuration...")
-    with open('config.json', 'r') as config_file:
+    with open('config_hlr.json', 'r') as config_file:
         config = json.load(config_file)
     print_configuration(config)
 
