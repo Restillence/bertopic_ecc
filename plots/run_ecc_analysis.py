0d64e8149fe4b134dd5612ff6f9915db592a1586
Melius Vitae <65677840+Restillence@users.noreply.github.com>
2024-11-07 09:24:14 +0100

    created new plots ecc. now trying zeroshot training with huang topics to create final plot. 1st try 0.0 zeroshot minsim

--- plots/run_ecc_analysis.py
+++ plots/run_ecc_analysis.py
@@ -33,6 +33,7 @@ def main():
     
  
     # Perform data exploration and plotting
+    """
     ecc_explorer.plot_paragraph_length_distribution(results_df)
     ecc_explorer.plot_ecc_length_distribution(results_df)
     ecc_explorer.plot_ecc_length_by_company(results_df)
@@ -40,22 +41,23 @@ def main():
     ecc_explorer.plot_files_distribution(results_df)
     ecc_explorer.plot_average_ecc_length_per_company(results_df)
     ecc_explorer.plot_ecc_length_distribution_by_year(results_df)
-
+    """
     # Add new plot for average paragraph length distribution
     #ecc_explorer.plot_avg_paragraph_length_distribution(results_df)
     
     #
-    ecc_explorer.get_calls_with_highest_avg_paragraph_lengths(results_df) # only works with "sampling_mode": "random_company"; and 
+    #ecc_explorer.get_calls_with_highest_avg_paragraph_lengths(results_df) # only works with "sampling_mode": "random_company"; and 
     #"docoument_split": "paragraphs"!!!
-    calls_above_threshold, permcos_above_threshold, percentage_permcos = ecc_explorer.get_calls_above_2_std(results_df)
+    #calls_above_threshold, permcos_above_threshold, percentage_permcos = ecc_explorer.get_calls_above_2_std(results_df)
 
+    """
     # To inspect the filtered calls and permcos:
     print("Calls Above Threshold (first 5 rows):")
     print(calls_above_threshold[['call_id', 'permco', 'avg_paragraph_length']].head())
     
     print("Permcos with Calls Above Threshold:", permcos_above_threshold)
     print(f"Percentage of Permcos with Calls Above 2 Std Dev: {percentage_permcos:.2f}%")
-
+    """
     # Display additional stats
     num_unique_companies, calls_per_company, top5_avg_length, summary_stats_table = ecc_explorer.additional_descriptive_statistics(results_df)
     ecc_explorer.display_tables(
@@ -68,15 +70,15 @@ def main():
 
     # Initialize NLPPlotter
     nlp_plotter = NLPPlotter(config_path="config.json")
-    
+    nlp_plotter.plot_ngram_frequencies(results_df)
     # Generate NLP-related plots
     #nlp_plotter.plot_tfidf_top_terms(results_df)
     #nlp_plotter.plot_topics_tsne_pca(results_df)
     #nlp_plotter.plot_sentiment_analysis(results_df)
-    nlp_plotter.plot_keyword_cooccurrence(results_df)
-    nlp_plotter.plot_word_length_distribution(results_df)
-    nlp_plotter.plot_bag_of_words(results_df)
-    nlp_plotter.plot_wordcloud(results_df)
+    #nlp_plotter.plot_keyword_cooccurrence(results_df)
+    #nlp_plotter.plot_word_length_distribution(results_df)
+    #nlp_plotter.plot_bag_of_words(results_df)
+    #nlp_plotter.plot_wordcloud(results_df)
 
 if __name__ == "__main__":
     main()
