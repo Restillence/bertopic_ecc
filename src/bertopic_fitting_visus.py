2b268315f058314cc240046377ae6f037091b7d3
Melius Vitae <65677840+Restillence@users.noreply.github.com>
2024-11-11 16:25:10 +0100

    .

--- src/bertopic_fitting_visus.py
+++ src/bertopic_fitting_visus.py
@@ -411,7 +411,7 @@ def main():
 
     # Load configuration from config.json
     print("Loading configuration...")
-    with open('config.json', 'r') as f:
+    with open('config_hlr.json', 'r') as f:
         config = json.load(f)
     print_configuration(config)
 
